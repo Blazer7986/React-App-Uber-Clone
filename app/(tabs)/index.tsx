ab8c846fcac3ff7c6f4ed4bd369acd57aaf11c2b
Vianey Rivera <vianeyr8624@gmail.com>
2025-08-04 20:02:01 -0400

    Install ESLint and Prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className={"text-red-500"}>Welcome to Nativewind!</Text>
+      <Text>Uber Clone</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
