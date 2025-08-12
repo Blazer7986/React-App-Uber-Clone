e736df2a00295a4bb82fa935e4de5db92f3cddaf
Vianey Rivera <vianeyr8624@gmail.com>
2025-08-12 17:59:08 -0400

    NeonDB implemented and  API routes

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
 import { useSignUp } from "@clerk/clerk-expo";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
@@ -55,7 +56,15 @@ const SignUp = () => {
       // If verification was completed, set the session to active
       // and redirect the user
       if (signUpAttempt.status === "complete") {
-        // TODO: Create a database user
+        await fetchAPI("/api/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdUserId,
+          }),
+        });
+
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({ ...verification, state: "success" });
       } else {
@@ -75,22 +84,6 @@ const SignUp = () => {
       });
     }
   };
-
-  // if (verification.state === "pending") {
-  //   return (
-  //     <>
-  //       <Text>Verify your email</Text>
-  //       <TextInput
-  //         value={verification.code}
-  //         placeholder="Enter your verification code"
-  //         onChangeText={(code) => setVerification({ ...verification, code })}
-  //       />
-  //       <TouchableOpacity onPress={onVerifyPress}>
-  //         <Text>Verify</Text>
-  //       </TouchableOpacity>
-  //     </>
-  //   );
-  // }
 
   return (
     <ScrollView className="flex-1 bg-white">
