2b520f16c819db825373c03d888cbb3e63a6c782
Vianey Rivera <vianeyr8624@gmail.com>
2025-08-15 14:15:13 -0400

    install and implement stripe payment

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,24 +1,24 @@
+import { router } from "expo-router";
+import { FlatList, View } from "react-native";
+
 import CustomButton from "@/components/CustomButton";
 import DriverCard from "@/components/DriverCard";
 import RideLayout from "@/components/RideLayout";
 import { useDriverStore } from "@/store";
-import { router } from "expo-router";
-import React from "react";
-import { View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
 
 const ConfirmRide = () => {
   const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
 
   return (
-    <RideLayout title="Choose Driver" snapPoints={["65%", "85%"]}>
+    <RideLayout title={"Choose a Rider"} snapPoints={["65%", "85%"]}>
       <FlatList
         data={drivers}
-        renderItem={({ item }) => (
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
           <DriverCard
-            selected={selectedDriver!}
-            setSelected={() => setSelectedDriver(Number(item.id!))}
             item={item}
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id!)}
           />
         )}
         ListFooterComponent={() => (
